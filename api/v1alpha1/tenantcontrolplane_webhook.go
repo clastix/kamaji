190acc99b305329a848e5fecb16d534d7198d415
Dario Tranchitella <dario@tranchitella.eu>
2022-11-26 17:43:54 +0100

    feat: tcp version validation upon create and update

--- api/v1alpha1/tenantcontrolplane_webhook.go
+++ api/v1alpha1/tenantcontrolplane_webhook.go
@@ -4,10 +4,18 @@
 package v1alpha1
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/blang/semver"
+	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	"github.com/clastix/kamaji/internal/config"
+	"github.com/clastix/kamaji/internal/upgrade"
 )
 
 // log is for logging in this package.
@@ -19,44 +27,84 @@ func (in *TenantControlPlane) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
 //+kubebuilder:webhook:path=/mutate-kamaji-clastix-io-v1alpha1-tenantcontrolplane,mutating=true,failurePolicy=fail,sideEffects=None,groups=kamaji.clastix.io,resources=tenantcontrolplanes,verbs=create;update,versions=v1alpha1,name=mtenantcontrolplane.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Defaulter = &TenantControlPlane{}
 
 // Default implements webhook.Defaulter so a webhook will be registered for the type.
 func (in *TenantControlPlane) Default() {
-	tenantcontrolplanelog.Info("default", "name", in.Name)
-
-	// TODO(user): fill in your defaulting logic.
+	if len(in.Spec.DataStore) == 0 {
+		in.Spec.DataStore = config.Config().GetString("datastore")
+	}
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 //+kubebuilder:webhook:path=/validate-kamaji-clastix-io-v1alpha1-tenantcontrolplane,mutating=false,failurePolicy=fail,sideEffects=None,groups=kamaji.clastix.io,resources=tenantcontrolplanes,verbs=create;update,versions=v1alpha1,name=vtenantcontrolplane.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &TenantControlPlane{}
 
+func normalizeKubernetesVersion(input string) string {
+	if strings.HasPrefix(input, "v") {
+		return strings.Replace(input, "v", "", 1)
+	}
+
+	return input
+}
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type.
 func (in *TenantControlPlane) ValidateCreate() error {
-	tenantcontrolplanelog.Info("validate create", "name", in.Name)
+	tenantcontrolplanelog.Info("validate create", "name", in.Name, "namespace", in.Namespace)
 
-	// TODO(user): fill in your validation logic upon object creation.
+	ver, err := semver.New(normalizeKubernetesVersion(in.Spec.Kubernetes.Version))
+	if err != nil {
+		return errors.Wrap(err, "unable to parse the desired Kubernetes version")
+	}
+
+	supportedVer, supportedErr := semver.Make(normalizeKubernetesVersion(upgrade.KubeadmVersion))
+	if supportedErr != nil {
+		return errors.Wrap(supportedErr, "unable to parse the Kamaji supported Kubernetes version")
+	}
+
+	if ver.GT(supportedVer) {
+		return fmt.Errorf("unable to create a TenantControlPlane with a Kubernetes version greater than the supported one, actually %s", supportedVer.String())
+	}
+
 	return nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type.
 func (in *TenantControlPlane) ValidateUpdate(old runtime.Object) error {
-	tenantcontrolplanelog.Info("validate update", "name", in.Name)
+	tenantcontrolplanelog.Info("validate update", "name", in.Name, "namespace", in.Namespace)
 
-	// TODO(user): fill in your validation logic upon object update.
+	o := old.(*TenantControlPlane) //nolint:forcetypeassert
+
+	oldVer, oldErr := semver.Make(normalizeKubernetesVersion(o.Spec.Kubernetes.Version))
+	if oldErr != nil {
+		return errors.Wrap(oldErr, "unable to parse the previous Kubernetes version")
+	}
+
+	newVer, newErr := semver.New(normalizeKubernetesVersion(in.Spec.Kubernetes.Version))
+	if newErr != nil {
+		return errors.Wrap(newErr, "unable to parse the desired Kubernetes version")
+	}
+
+	supportedVer, supportedErr := semver.Make(normalizeKubernetesVersion(upgrade.KubeadmVersion))
+	if supportedErr != nil {
+		return errors.Wrap(supportedErr, "unable to parse the Kamaji supported Kubernetes version")
+	}
+
+	switch {
+	case newVer.GT(supportedVer):
+		return fmt.Errorf("unable to upgrade to a version greater than the supported one, actually %s", supportedVer.String())
+	case newVer.LT(oldVer):
+		return fmt.Errorf("unable to downgrade a TenantControlPlane from %s to %s", oldVer.String(), newVer.String())
+	case newVer.Minor-oldVer.Minor > 1:
+		return fmt.Errorf("unable to upgrade to a minor version in a non-sequential mode")
+	}
+
 	return nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type.
 func (in *TenantControlPlane) ValidateDelete() error {
-	tenantcontrolplanelog.Info("validate delete", "name", in.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }
--- go.mod
+++ go.mod
@@ -44,6 +44,7 @@ require (
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/blang/semver v3.5.1+incompatible // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
 	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
