dbe5fef3bf88275fe3b4351f152739fa747088d9
Simon Kienzler <simon.kienzler@inovex.de>
2024-08-21 15:34:59 +0200

    test: Add tests for defaulting webhook

---
+++ internal/webhook/handlers/handlers_suite_test.go
@@ -0,0 +1,16 @@
+// Copyright 2024 Clastix Labs
+// SPDX-License-Identifier: Apache-2.0
+
+package handlers_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestHandlers(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Handlers Suite")
+}
