fa0b5c1a7033602fbc1dd189e31aeca94175155b
Alfredo Suarez <asuarezaceves@gmail.com>
2025-11-01 10:35:02 +0100

    latest code POC Working but e2e fails

--- e2e/tcp_gateway_ready_test.go
+++ e2e/tcp_gateway_ready_test.go
@@ -5,13 +5,13 @@ package e2e
 
 import (
 	"context"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	pointer "k8s.io/utils/ptr"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 
 	kamajiv1alpha1 "github.com/clastix/kamaji/api/v1alpha1"
@@ -66,22 +66,18 @@ var _ = Describe("Deploy a TenantControlPlane with Gateway API", func() {
 		},
 	}
 
-	// Create a TenantControlPlane resource into the cluster
 	JustBeforeEach(func() {
 		Expect(k8sClient.Create(context.Background(), tcp)).NotTo(HaveOccurred())
 	})
 
-	// Delete the TenantControlPlane resource after test is finished
 	JustAfterEach(func() {
 		Expect(k8sClient.Delete(context.Background(), tcp)).Should(Succeed())
 	})
 
-	// Check if TenantControlPlane resource has been created
 	It("Should be Ready", func() {
 		StatusMustEqualTo(tcp, kamajiv1alpha1.VersionReady)
 	})
 
-	// Check if HTTPRoute has been created
 	It("Should create HTTPRoute resource", func() {
 		Eventually(func() error {
 			httpRoute := &gatewayv1.HTTPRoute{}
@@ -89,7 +85,7 @@ var _ = Describe("Deploy a TenantControlPlane with Gateway API", func() {
 				Name:      tcp.Name,
 				Namespace: tcp.Namespace,
 			}, httpRoute)
-		}, "2m", "10s").Should(Succeed())
+		}).WithTimeout(time.Minute).Should(Succeed())
 	})
 
 	// Check if GRPCRoute has been created
@@ -100,107 +96,6 @@ var _ = Describe("Deploy a TenantControlPlane with Gateway API", func() {
 				Name:      tcp.Name,
 				Namespace: tcp.Namespace,
 			}, grpcRoute)
-		}, "2m", "10s").Should(Succeed())
-	})
-
-	// // Check if HTTPRoute has correct configuration
-	// It("Should have correct HTTPRoute configuration", func() {
-	// 	var httpRoute *gatewayv1.HTTPRoute
-
-	// 	Eventually(func() error {
-	// 		httpRoute = &gatewayv1.HTTPRoute{}
-	// 		return k8sClient.Get(context.Background(), types.NamespacedName{
-	// 			Name:      tcp.Name,
-	// 			Namespace: tcp.Namespace,
-	// 		}, httpRoute)
-	// 	}, "5m", "10s").Should(Succeed())
-
-	// 	// Check labels
-	// 	Expect(httpRoute.Labels).To(HaveKeyWithValue("test.kamaji.io/gateway", "true"))
-	// 	Expect(httpRoute.Labels).To(HaveKeyWithValue("app.kubernetes.io/name", "kamaji"))
-
-	// 	// Check annotations
-	// 	Expect(httpRoute.Annotations).To(HaveKeyWithValue("test.kamaji.io/created-by", "e2e-test"))
-
-	// 	// Check parent references
-	// 	Expect(httpRoute.Spec.ParentRefs).To(HaveLen(1))
-	// 	Expect(httpRoute.Spec.ParentRefs[0].Name).To(Equal(gatewayv1.ObjectName("test-gateway")))
-
-	// 	// Check hostname
-	// 	Expect(httpRoute.Spec.Hostnames).To(ContainElement(gatewayv1.Hostname("tcp-gateway.example.com")))
-
-	// 	// Check that it has backend rules
-	// 	Expect(httpRoute.Spec.Rules).To(HaveLen(1))
-	// 	Expect(httpRoute.Spec.Rules[0].BackendRefs).To(HaveLen(1))
-	// })
-
-	// // Check if GRPCRoute has correct configuration
-	// It("Should have correct GRPCRoute configuration", func() {
-	// 	var grpcRoute *gatewayv1.GRPCRoute
-
-	// 	Eventually(func() error {
-	// 		grpcRoute = &gatewayv1.GRPCRoute{}
-	// 		return k8sClient.Get(context.Background(), types.NamespacedName{
-	// 			Name:      tcp.Name,
-	// 			Namespace: tcp.Namespace,
-	// 		}, grpcRoute)
-	// 	}, "5m", "10s").Should(Succeed())
-
-	// 	// Check labels
-	// 	Expect(grpcRoute.Labels).To(HaveKeyWithValue("test.kamaji.io/gateway", "true"))
-	// 	Expect(grpcRoute.Labels).To(HaveKeyWithValue("app.kubernetes.io/name", "kamaji"))
-
-	// 	// Check annotations
-	// 	Expect(grpcRoute.Annotations).To(HaveKeyWithValue("test.kamaji.io/created-by", "e2e-test"))
-
-	// 	// Check parent references
-	// 	Expect(grpcRoute.Spec.ParentRefs).To(HaveLen(1))
-	// 	Expect(grpcRoute.Spec.ParentRefs[0].Name).To(Equal(gatewayv1.ObjectName("test-gateway")))
-
-	// 	// Check hostname
-	// 	Expect(grpcRoute.Spec.Hostnames).To(ContainElement(gatewayv1.Hostname("tcp-gateway.example.com")))
-
-	// 	// Check that it has backend rules
-	// 	Expect(grpcRoute.Spec.Rules).To(HaveLen(1))
-	// 	Expect(grpcRoute.Spec.Rules[0].BackendRefs).To(HaveLen(1))
-	// })
-
-	// Check cleanup when Gateway configuration is removed
-	Context("When Gateway configuration is removed", func() {
-		It("Should cleanup Gateway resources", func() {
-			// Wait for resources to be created first
-			Eventually(func() error {
-				httpRoute := &gatewayv1.HTTPRoute{}
-				return k8sClient.Get(context.Background(), types.NamespacedName{
-					Name:      tcp.Name,
-					Namespace: tcp.Namespace,
-				}, httpRoute)
-			}, "5m", "10s").Should(Succeed())
-
-			// Update TCP to remove Gateway configuration
-			Expect(k8sClient.Get(context.Background(), client.ObjectKeyFromObject(tcp), tcp)).To(Succeed())
-			tcp.Spec.ControlPlane.GatewayRoutes = nil
-			Expect(k8sClient.Update(context.Background(), tcp)).To(Succeed())
-
-			// Check that HTTPRoute is deleted
-			Eventually(func() bool {
-				httpRoute := &gatewayv1.HTTPRoute{}
-				err := k8sClient.Get(context.Background(), types.NamespacedName{
-					Name:      tcp.Name,
-					Namespace: tcp.Namespace,
-				}, httpRoute)
-				return err != nil
-			}, "2m", "5s").Should(BeTrue())
-
-			// Check that GRPCRoute is deleted
-			Eventually(func() bool {
-				grpcRoute := &gatewayv1.GRPCRoute{}
-				err := k8sClient.Get(context.Background(), types.NamespacedName{
-					Name:      tcp.Name,
-					Namespace: tcp.Namespace,
-				}, grpcRoute)
-				return err != nil
-			}, "2m", "5s").Should(BeTrue())
-		})
+		}).WithTimeout(time.Minute).Should(Succeed())
 	})
 })
