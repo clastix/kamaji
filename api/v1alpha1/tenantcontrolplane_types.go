5e576071f010baa587f8de9027b66324dcea7af5
Fabian Breckle <39595706+fbreckle@users.noreply.github.com>
2026-06-20 10:35:43 +0200

    feat: configurable securityContext for containers and pod (#1181)
    * feat: add securityContexts to control plane components
    
    * feat: add configurable security context to Konnectivity server component
    
    * test: add e2e test for tcp security contexts
    
    * test(refactor): use getControlPlanePods function where possible
    
    ---------
    
    Co-authored-by: Sebastian Diez <sebastian.diez@breuninger.de>

--- api/v1alpha1/tenantcontrolplane_types.go
+++ api/v1alpha1/tenantcontrolplane_types.go
@@ -252,6 +252,16 @@ type ControlPlaneProbes struct {
 	Scheduler *ProbeSet `json:"scheduler,omitempty"`
 }
 
+// ControlPlaneContainerSecurityContexts defines security contexts for individual Control Plane component containers.
+type ControlPlaneContainerSecurityContexts struct {
+	// APIServer defines the security context for the kube-apiserver container.
+	APIServer *corev1.SecurityContext `json:"apiServer,omitempty"`
+	// ControllerManager defines the security context for the kube-controller-manager container.
+	ControllerManager *corev1.SecurityContext `json:"controllerManager,omitempty"`
+	// Scheduler defines the security context for the kube-scheduler container.
+	Scheduler *corev1.SecurityContext `json:"scheduler,omitempty"`
+}
+
 type DeploymentSpec struct {
 	// RegistrySettings allows to override the default images for the given Tenant Control Plane instance.
 	// It could be used to point to a different container registry rather than the public one.
@@ -310,6 +320,10 @@ type DeploymentSpec struct {
 	//+kubebuilder:default="default"
 	// ServiceAccountName allows to specify the service account to be mounted to the pods of the Control plane deployment
 	ServiceAccountName string `json:"serviceAccountName,omitempty"`
+	// ContainerSecurityContexts allows to specify the security context for the individual control plane components.
+	ContainerSecurityContexts *ControlPlaneContainerSecurityContexts `json:"containerSecurityContexts,omitempty"`
+	// PodSecurityContext allows to specify the security context for the control plane pod.
+	PodSecurityContext *corev1.PodSecurityContext `json:"podSecurityContext,omitempty"`
 }
 
 // AdditionalVolumeMounts allows mounting additional volumes to the Control Plane components.
@@ -371,6 +385,8 @@ type KonnectivityServerSpec struct {
 	// Resources define the amount of CPU and memory to allocate to the Konnectivity server.
 	Resources *corev1.ResourceRequirements `json:"resources,omitempty"`
 	ExtraArgs ExtraArgs                    `json:"extraArgs,omitempty"`
+	// SecurityContext defines the SecurityContext for the Konnectivity server container.
+	SecurityContext *corev1.SecurityContext `json:"securityContext,omitempty"`
 }
 
 type KonnectivityAgentMode string
