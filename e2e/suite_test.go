d7e47c4daf90990782b6dca60724737930ecf7f1
Ross Golder <ross@golder.org>
2026-07-27 20:28:23 +0700

    revert: remove unrelated e2e infrastructure changes from PR

--- e2e/suite_test.go
+++ e2e/suite_test.go
@@ -9,7 +9,6 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
@@ -77,11 +76,7 @@ var _ = BeforeSuite(func() {
 			ControllerName: "gateway.envoyproxy.io/gatewayclass-controller",
 		},
 	}
-	err = k8sClient.Create(context.Background(), gatewayClass)
-
-	if err != nil && !apierrors.IsAlreadyExists(err) {
-		Expect(err).NotTo(HaveOccurred())
-	}
+	Expect(k8sClient.Create(context.Background(), gatewayClass)).NotTo(HaveOccurred())
 
 	By("creating Gateway with kube-apiserver and konnectivity-server listeners")
 	CreateGatewayWithListeners("test-gateway", "default", "envoy-gw-class", "*.example.com")
