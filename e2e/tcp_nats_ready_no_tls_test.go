1991a14bf030b0ec4ca7f0aba4e324ce70f69b8a
Ross Golder <ross@golder.org>
2026-08-05 09:05:14 +0700

    chore: gci import formatting

--- e2e/tcp_nats_ready_no_tls_test.go
+++ e2e/tcp_nats_ready_no_tls_test.go
@@ -6,12 +6,10 @@ package e2e
 import (
 	"context"
 
+	kamajiv1alpha1 "github.com/clastix/kamaji/api/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	pointer "k8s.io/utils/ptr"
-
-	kamajiv1alpha1 "github.com/clastix/kamaji/api/v1alpha1"
 )
 
 var _ = Describe("Deploy a TenantControlPlane resource with the NATS driver without TLS", func() {
@@ -25,7 +23,7 @@ var _ = Describe("Deploy a TenantControlPlane resource with the NATS driver with
 			DataStore: "nats-notls",
 			ControlPlane: kamajiv1alpha1.ControlPlane{
 				Deployment: kamajiv1alpha1.DeploymentSpec{
-					Replicas: pointer.To(int32(1)),
+					Replicas: new(int32(1)),
 				},
 				Service: kamajiv1alpha1.ServiceSpec{
 					ServiceType: "ClusterIP",
--- e2e/tcp_scale_test.go
+++ e2e/tcp_scale_test.go
@@ -6,12 +6,10 @@ package e2e
 import (
 	"context"
 
+	kamajiv1alpha1 "github.com/clastix/kamaji/api/v1alpha1"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	pointer "k8s.io/utils/ptr"
-
-	kamajiv1alpha1 "github.com/clastix/kamaji/api/v1alpha1"
 )
 
 var _ = Describe("Scale a TenantControlPlane resource", func() {
@@ -24,7 +22,7 @@ var _ = Describe("Scale a TenantControlPlane resource", func() {
 		Spec: kamajiv1alpha1.TenantControlPlaneSpec{
 			ControlPlane: kamajiv1alpha1.ControlPlane{
 				Deployment: kamajiv1alpha1.DeploymentSpec{
-					Replicas: pointer.To(int32(1)),
+					Replicas: new(int32(1)),
 				},
 				Service: kamajiv1alpha1.ServiceSpec{
 					ServiceType: "ClusterIP",
