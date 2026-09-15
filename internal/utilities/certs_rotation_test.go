e092d0853ef205efb96d6ee3a61515b917db07ee
Ross Golder <ross@golder.org>
2026-09-15 18:52:35 +0700

    chore: fix import ordering in certs_rotation_test.go (gci)

--- internal/utilities/certs_rotation_test.go
+++ internal/utilities/certs_rotation_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestIsRotationRequested(t *testing.T) {
